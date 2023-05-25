3b85f45d0809059878b15438de1d31aa35610f02
Annie P <elizabethpaulukonis@gmail.com>
2023-05-25 15:21:43 -0400

    add code for fixing beetool data (fread)

---
+++ src/00_setup.R
@@ -0,0 +1,47 @@
+### B. affinis analysis
+
+### 00 Setup of directories and file paths
+
+# Edited by E. Paulukonis October 2022
+
+library(sp)
+library(sf)
+library(stars)
+library(rgeos)
+library(rgdal)
+library(terra)
+library(raster)
+library(stars)
+library(abind)
+library(dplyr)
+library(ggplot2)
+library(cowplot)
+library(grid)
+library(foreign)
+library(progress)
+library(parallel)
+library(foreach)
+library(gridExtra)
+library(stringr)
+library(smoothr)
+library(exactextractr)
+library(data.table)
+library(gtools)
+library(gstat)
+
+
+who_is_running<-'eap'
+#who_is_running<-'stp'
+if(Sys.info()[4]=="LZ2626UTPURUCKE"){
+  root_dir <- file.path("c:", "git", "pollinator_probabilistic_loading")
+}else if (Sys.info()[4]=="LZ26EPAULUKO"){
+  root_dir <- 'C:/Users/EPAULUKO/OneDrive - Environmental Protection Agency (EPA)/Profile/Documents/GitHub/non-apis-diversity-exposure'
+}else{
+  root_dir <- file.path("/work", "HONEYBEE", who_is_running, "non-apis-diversity-exposure")
+}
+print(root_dir)
+
+memory.limit(size=56000)
+
+root_data_in <- file.path(root_dir, "data")
+root_src <- file.path(root_dir, "src")
---
+++ src/01_process_datasets.R
@@ -0,0 +1,8 @@
+
+#### code for processing and analyzing the beetool dataset
+
+file<-(paste0(root_dir,"/beetoolALL2023.csv"))
+beetool<-data.table::fread(file)
+head(beetool)
+
+
